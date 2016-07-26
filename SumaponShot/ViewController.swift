5ce49318619acb164806931c828df34130701aa2
elzup <guild0105@gmail.com>
2016-07-26 12:44:44 +0900

    Initial Commit

---
+++ SumaponShot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SumaponShot
+//
+//  Created by Hiroto Takahashi on 2016/07/26.
+//  Copyright © 2016年 Hiroto Takahashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
